c49ad99583fdbc8a602ac9434d6b5acfb1431d8b
jishuya <jishuya3015@gmail.com>
2025-09-04 12:33:33 +0900

    ✨ [feat] Add admin role

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
 import { login, logout, onUserStateChange } from '../api/firebase';
+import User from './User';
 
 export default function Navbar() {
 
@@ -10,17 +11,10 @@ export default function Navbar() {
 
   useEffect(() => {
     onUserStateChange((user) => {
-      console.log(user);
+      console.log('🚀', user);
       setUser(user);
     });
   }, []);
-
-  const handleLogin = () => {
-    login().then(setUser);
-  };
-  const handleLogout = () => {
-    logout().then(setUser);
-  };
 
   return (
     <header className='flex justify-between border-b border-gray-300 p-2'>
@@ -34,8 +28,9 @@ export default function Navbar() {
         <Link to='/products/new' className='text-2xl'>
           <BsFillPencilFill />
         </Link>
-        {!user && <button onClick={handleLogin}>Login</button>}
-        {user && <button onClick={handleLogout}>Logout</button>}
+        {user && <User user={user} />}
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logout}>Logout</button>}
       </nav>
     </header>
   );
